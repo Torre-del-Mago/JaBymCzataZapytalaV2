619e7ed3203f83fc7da484876b374d8960cbd45f
Bezimienii <czerminskijakub@gmail.com>
2024-05-13 23:18:31 +0200

    Added consumers for services that communicate with gateway

---
+++ Services/OfferCommand/Consumer/ReserveOfferConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+
+namespace OfferCommand.Consumer
+{
+    public class ReserveOfferConsumer : IConsumer<ReserveEvent>
+    {
+        public async Task Consume(ConsumeContext<ReserveEvent> context)
+        {
+            /*
+             Do something
+             */
+
+            await context.Publish(new ReserveReplyEvent() { })
+        }
+    }
+}
