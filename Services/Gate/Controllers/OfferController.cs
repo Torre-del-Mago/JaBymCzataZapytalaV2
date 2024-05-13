60304a998efebf6d9860a9efeaa245b5be19b5b3
Bezimienii <czerminskijakub@gmail.com>
2024-05-13 22:57:11 +0200

    Created example controllers in gateway

---
+++ Services/Gate/Controllers/OfferController.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gate.Controllers
+{
+    [ApiController]
+    [Route("api/offer/")]
+    public class OfferController
+    {
+        private IRequestClient<ReserveEvent> _requestClient { get; set; }
+        public OfferController(IRequestClient<ReserveEvent> reserveClient) { 
+        
+        }
+
+        [HttpGet("reserve")]
+        public bool Reserve() {
+            var request = new ReserveEvent() { };
+            var response = await _requestClient.GetResponse<ReserveReplyEvent>(request);
+            return response.Message.HasReserved;
+        }
+    }
+}
