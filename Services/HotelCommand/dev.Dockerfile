dff0f690827e2133688a4ecc4bad2d77c18312e5
KKucht <krzysztof.k2001.01@wp.pl>
2024-05-13 15:16:08 +0200

    Basic deployment and eventmodel

---
+++ Services/HotelCommand/dev.Dockerfile
@@ -0,0 +1,20 @@
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+COPY ["Services/HotelCommand/HotelCommand.csproj", "HotelCommand/"]
+RUN dotnet restore "./HotelCommand/HotelCommand.csproj"
+
+COPY Services/HotelCommand/. ./HotelCommand/
+RUN dotnet build "./HotelCommand/HotelCommand.csproj" -c Release -o /app/build -nowarn:CS8618
+
+FROM build AS publish
+RUN dotnet publish "./HotelCommand/HotelCommand.csproj" -c Release -o /app/publish --self-contained false --no-restore
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "HotelCommand.dll"]
---
+++ Services/Models/EventModel.cs
@@ -0,0 +1,25 @@
+namespace Models
+{
+    // Class to inherit after for differnt types of events
+    public class EventModel
+    {
+        public Guid Id { get; set; }
+        public Guid CorrelationId { get; set; }
+        public DateTime CreationDate { get; private set; }
+        public EventModel(Guid id, DateTime date, Guid cId)
+        {
+            Id = id;
+            CreationDate = date;
+            CorrelationId = cId;
+        }
+
+        public EventModel()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+            CorrelationId = Guid.NewGuid();
+        }
+    }
+}
+
+// https://github.com/dotnet-architecture/eShopOnContainers/blob/main/src/BuildingBlocks/EventBus/EventBus/Events/IntegrationEvent.cs
