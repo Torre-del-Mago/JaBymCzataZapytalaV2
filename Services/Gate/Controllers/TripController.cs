4478ce1b2d5c708f96a9a88c17d788392eb0148b
Bezimienii <czerminskijakub@gmail.com>
2024-05-14 20:47:08 +0200

    Created communication for calculating price

--- Services/Gate/Controllers/TripController.cs
+++ Services/Gate/Controllers/TripController.cs
@@ -10,11 +10,13 @@ namespace Gate.Controllers
     {
         private IRequestClient<GenerateTripEvent> _tripRequestClient { get; set; }
         private IRequestClient<GenerateTripsEvent> _tripListRequestClient { get; set; }
+        private IRequestClient<CalculatePriceEvent> _calculatePriceClient { get; set; }
         public TripController(IRequestClient<GenerateTripEvent> tripRequestClient,
-            IRequestClient<GenerateTripsEvent> tripListRequestClient) { 
+            IRequestClient<GenerateTripsEvent> tripListRequestClient,
+            IRequestClient<CalculatePriceEvent> calculatePriceClient) { 
             _tripRequestClient = tripRequestClient;
             _tripListRequestClient = tripListRequestClient;
-        
+            _calculatePriceClient = calculatePriceClient;
         }
 
         [HttpGet("trip-info")]
@@ -38,6 +40,19 @@ namespace Gate.Controllers
             var response = await _tripRequestClient.GetResponse<GenerateTripsEventReply>(request);
             return response.Message.TripInfo;
         }
+
+        [HttpGet("calculate-price")]
+        public async float getTripPrice()
+        {
+            var request = new CalculatePriceEvent()
+            {
+
+            };
+            var response = await _tripRequestClient.GetResponse<CalculatePriceEventReply>(request);
+            return response.Message.TripInfo;
+        }
+
+
 
         /*
          !!!!! TODO: Calculate price endpoint
