bf6fcf23d8f600dccec74010b2fd7147b281d468
Agata Geisler <geisler.agata@wp.pl>
2024-05-15 15:37:23 +0200

    Added noSQL database for HotelQuery

---
+++ Services/HotelQuery/Database/Entity/Diet.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace HotelQuery.Database.Entity;
+
+public class Diet
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+    
+    [BsonElement("name")]
+    public string Name { get; set; }
+}
