6da5916f6097abd456f2130cc472933ebe24e251
KKucht <krzysztof.k2001.01@wp.pl>
2024-05-15 17:27:11 +0200

    Add Event to Base

--- Services/HotelCommand/Database/HotelContext.cs
+++ Services/HotelCommand/Database/HotelContext.cs
@@ -1,6 +1,32 @@
-﻿namespace HotelCommand.Database;
+﻿using HotelCommand.Database.Tables;
+using Microsoft.EntityFrameworkCore;
 
-public class HotelContext 
+namespace HotelCommand.Database
 {
-    
+    public class HotelContext : DbContext
+    {
+        public DbSet<Diet> Diets { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<HotelDiet> HotelDiets { get; set; }
+
+        public DbSet<HotelRoomType> HotelRoomTypes { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+        public DbSet<ReservedRoom> ReservedRooms { get; set; }
+
+        public DbSet<RoomType> RoomTypes { get; set; }
+
+        private readonly IConfiguration configuration;
+
+        public HotelContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(configuration.GetConnectionString("DbPath"));
+    }
 }
