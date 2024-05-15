2e0d35648ba66e7c096341f6774e2f13defefedb
Agata Geisler <geisler.agata@wp.pl>
2024-05-15 15:43:39 +0200

    Fixed namespaces

--- Services/HotelCommand/Database/Tables/ReservedRoom.cs
+++ Services/HotelCommand/Database/Tables/ReservedRoom.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace HotelQuery.Database.Tables;
+namespace HotelCommand.Database.Tables;
 
 public class ReservedRoom
 {
