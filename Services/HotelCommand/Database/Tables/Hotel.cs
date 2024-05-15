cd8f710737dc4606ad6134df40ea81ba0fd3d547
Agata Geisler <geisler.agata@wp.pl>
2024-05-15 14:56:28 +0200

    Added SQL database for HotelCommand

---
+++ Services/HotelCommand/Database/Tables/Hotel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelQuery.Database.Tables;
+
+public class Hotel
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    public float Discount { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    
+    public List<Diet> Diets { get; set; }
+    public List<RoomType> RoomTypes { get; set; }
+}
