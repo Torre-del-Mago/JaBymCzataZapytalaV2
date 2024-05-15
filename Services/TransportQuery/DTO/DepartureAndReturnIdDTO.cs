e1ec75c9017fcaf0c4a3fdb1e22fcc2ca4d699bf
Bezimienii <czerminskijakub@gmail.com>
2024-05-15 20:21:44 +0200

    Created dtos for fetching for singular trip

---
+++ Services/TransportQuery/DTO/DepartureAndReturnIdDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TransportQuery.DTO
+{
+    public class DepartureAndReturnIdDTO
+    {
+        public string DepartureId { get; set; }
+        public string ReturnId { get; set; }   
+        
+        //for both departure and return
+        public float Price { get; set; }
+    }
+}
