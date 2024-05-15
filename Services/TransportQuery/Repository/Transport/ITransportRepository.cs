c09033c2356198e7e71472a0281695fd05aa3504
Bezimienii <czerminskijakub@gmail.com>
2024-05-15 20:22:14 +0200

    Created service and repository for singular trip.

---
+++ Services/TransportQuery/Repository/Transport/ITransportRepository.cs
@@ -0,0 +1,14 @@
+﻿using TransportQuery.DTO;
+
+namespace TransportQuery.Repository.Transport
+{
+    public interface ITransportRepository
+    {
+        public List<ConnectionDTO> getConnectionGoingTo(string destinationCity);
+        public List<ConnectionDTO> getConnectionComingFrom(string destinationCity);
+
+        public List<TransportDTO> getTransportsForConnections(string destinationIds);
+
+        public int getNumberOfTakenSeatsForTransport(string transportId);
+    }
+}
