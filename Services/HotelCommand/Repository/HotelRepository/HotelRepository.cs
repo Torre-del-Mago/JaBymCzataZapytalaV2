1f2fa82c2228069ec4a698facf8e2757a4f53788
KKucht <krzysztof.k2001.01@wp.pl>
2024-05-15 23:41:17 +0200

    Add Hotel Repos in command

---
+++ Services/HotelCommand/Repository/HotelRepository/HotelRepository.cs
@@ -0,0 +1,14 @@
+﻿using HotelCommand.Database;
+
+namespace HotelCommand.Repository.HotelRepository
+{
+    public class HotelRepository : IHotelRepository
+    {
+        private HotelContext _context;
+
+        public HotelRepository(HotelContext context)
+        {
+            _context = context;
+        }
+    }
+}
