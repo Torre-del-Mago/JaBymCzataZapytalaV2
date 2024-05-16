69af216562bf9757932759fec8e5021de11bc4c7
Bezimienii <czerminskijakub@gmail.com>
2024-05-16 22:49:03 +0200

    Added missing route imports and link to login

--- Frontend/frontend/src/app/app-routing.module.ts
+++ Frontend/frontend/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchComponent} from "./search/search.component"
+import {LoginComponent} from "./login/login.component"
+import {DetailComponent} from "./detail/detail.component"
 
 const routes: Routes = [
   {path: '', component: SearchComponent},
