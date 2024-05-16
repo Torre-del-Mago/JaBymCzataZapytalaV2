868cfdcdfa06843fdf5b7eaf63660b17ad4ab810
Agata Geisler <geisler.agata@wp.pl>
2024-05-16 16:04:08 +0200

    Added entity for hotel rooms

---
+++ Services/HotelQuery/Database/Entity/HotelRoomType.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace HotelQuery.Database.Entity;
+
+public class HotelRoom
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+    
+    [BsonElement("hotelId")]
+    public Guid HotelId { get; set; }
+    
+    [BsonElement("numberOfPeople")]
+    public int NumberOfPeople { get; set; }
+    
+    [BsonElement("city")]
+    public string City { get; set; }
+}
