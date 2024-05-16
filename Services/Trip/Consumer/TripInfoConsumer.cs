c036c75db8df844c80c58ab9d9941292a84f15d0
Agata Geisler <geisler.agata@wp.pl>
2024-05-16 19:16:18 +0200

    Added creation of the TripDTO and TripsDTO to publish

--- Services/Trip/Consumer/TripInfoConsumer.cs
+++ Services/Trip/Consumer/TripInfoConsumer.cs
@@ -3,6 +3,7 @@ using MassTransit.Clients;
 using Models.Hotel;
 using Models.Transport;
 using Models.Trip;
+using Models.Trip.DTO;
 
 namespace Trip.Consumer
 {
@@ -11,24 +12,37 @@ namespace Trip.Consumer
         private IRequestClient<GetHotelDataForTripEvent> _hotelClient { get; set; } 
         private IRequestClient<GetTransportDataForTripEvent> _transportClient { get; set; } 
         public TripInfoConsumer(IRequestClient<GetHotelDataForTripEvent> hotelClient,
-            IRequestClient<GetTransportDataForTripEvent> transportClient
-            ) { 
+            IRequestClient<GetTransportDataForTripEvent> transportClient) 
+        { 
             _hotelClient = hotelClient;
             _transportClient = transportClient;
         }
 
         public async Task Consume(ConsumeContext<GenerateTripEvent> context)
         {
-
             var hotelRequest = new GetHotelDataForTripEvent() { };
             var transportRequest = new GetTransportDataForTripEvent() { };
             var hotelResponse = await _hotelClient.GetResponse<GetHotelDataForTripEventReply>(hotelRequest);
             var transportResponse = await _hotelClient.GetResponse<GetTransportDataForTripEventReply>(transportRequest);
 
-            /*
-             Do something
-             */
-            await context.Publish(new GenerateTripEventReply() { });
+            var hotelDto = hotelResponse.Message.Hotel;
+            var transportDto = transportResponse.Message.Transport;
+
+            var tripDto = new TripDTO
+            {
+                HotelName = hotelDto.HotelName,
+                Country = hotelDto.Country,
+                City = hotelDto.City,
+                BeginDate = hotelDto.BeginDate,
+                EndDate = hotelDto.EndDate,
+                TypesOfMeals = hotelDto.TypesOfMeals,
+                Rooms = hotelDto.Rooms,
+                Discount = hotelDto.Discount,
+                ChosenFlight = transportDto.ChosenFlight,
+                PossibleFlights = transportDto.PossibleFlights
+            };
+
+            await context.Publish(new GenerateTripEventReply() {TripDTO = tripDto});
         }
     }
 }
--- Services/Trip/Consumer/TripListInfoConsumer.cs
+++ Services/Trip/Consumer/TripListInfoConsumer.cs
@@ -1,7 +1,10 @@
 ﻿using MassTransit;
 using Models.Hotel;
+using Models.Hotel.DTO;
 using Models.Transport;
+using Models.Transport.DTO;
 using Models.Trip;
+using Models.Trip.DTO;
 
 namespace Trip.Consumer
 {
@@ -19,14 +22,85 @@ namespace Trip.Consumer
 
         public async Task Consume(ConsumeContext<GenerateTripsEvent> context)
         {
+            var criteria = context.Message.Criteria;
+            if (criteria == null)
+            {
+                context.Message.Criteria = new CriteriaForTrips
+                {
+                    NrOfPeople = 2,
+                    Country = "Grecja",
+                    BeginDate = DateTime.Today,
+                    EndDate = DateTime.Today.AddDays(7),
+                    Departure = "Gdańsk"
+                };
+            }
+            if ((criteria.BeginDate == null) || (criteria.EndDate == null))
+            {
+                context.Message.Criteria.BeginDate = DateTime.Today;
+                context.Message.Criteria.EndDate = DateTime.Today.AddDays(7);
+            }
+            if ((criteria.NrOfPeople <= 0) || (criteria.NrOfPeople == null))
+            {
+                context.Message.Criteria.NrOfPeople = 2;
+            }
+            if (criteria.Country == null)
+            {
+                context.Message.Criteria.Country = "Grecja";
+            }
+            if (criteria.Departure == null)
+            {
+                context.Message.Criteria.Departure = "Gdańsk";
+            }
+            
             var hotelRequest = new GetHotelDataForTripsEvent() { };
             var transportRequest = new GetTransportDataForTripsEvent() { };
             var hotelResponse = await _hotelClient.GetResponse<GetHotelDataForTripsEventReply>(hotelRequest);
             var transportResponse = await _hotelClient.GetResponse<GetTransportDataForTripsEventReply>(transportRequest);
-            /*
-             Do something
-             */
-            await context.Publish(new GenerateTripsEventReply() { });
+            
+            var hotelsDto = hotelResponse.Message.Hotels.Hotels;
+            var transportsDto = transportResponse.Message.Transports.Transports;
+            
+            var matchingTrips = new List<Tuple<HotelDTO, TransportDTO>>();
+            foreach (var hotelDto in hotelsDto)
+            {
+                foreach (var transportDto in transportsDto)
+                {
+                    if (hotelDto.Country == transportDto.Destination && hotelDto.City == transportDto.Destination)
+                    {
+                        matchingTrips.Add(Tuple.Create(hotelDto, transportDto));
+                    }
+                }
+            }
+            
+            var random = new Random();
+            var selectedTrips = matchingTrips.OrderBy(x => random.Next()).Take(10);
+
+            var trips = new List<TripDTO>();
+
+            foreach (var tripTuple in selectedTrips)
+            {
+                var hotelDto = tripTuple.Item1;
+                var transportDto = tripTuple.Item2;
+                
+                var trip = new TripDTO
+                {
+                    HotelName = hotelDto.HotelName,
+                    Country = hotelDto.Country,
+                    City = hotelDto.City,
+                    BeginDate = hotelDto.BeginDate,
+                    EndDate = hotelDto.EndDate,
+                    TypesOfMeals = hotelDto.TypesOfMeals,
+                    Rooms = hotelDto.Rooms,
+                    Discount = hotelDto.Discount,
+                    ChosenFlight = transportDto.ChosenFlight,
+                    PossibleFlights = transportDto.PossibleFlights
+                };
+
+                trips.Add(trip);
+            }
+
+            var tripsDto = new TripsDTO() {Trips = trips};
+            await context.Publish(new GenerateTripsEventReply() {Trips = tripsDto});
         }
     }
 }
