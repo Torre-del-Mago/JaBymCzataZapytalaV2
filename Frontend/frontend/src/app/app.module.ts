5a5391f4632a75ba7d2fa3f44d2c9e7d01e654f9
Bezimienii <czerminskijakub@gmail.com>
2024-05-16 22:04:58 +0200

    Added project files and views

---
+++ Frontend/frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DetailComponent,
+    LoginComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
