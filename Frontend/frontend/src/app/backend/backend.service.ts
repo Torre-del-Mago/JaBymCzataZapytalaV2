773c3b0e3d3d39adf8c673ead2a34fa0537c7d4e
Bezimienii <czerminskijakub@gmail.com>
2024-05-16 23:36:29 +0200

    Added urls for gate communication

--- Frontend/frontend/src/app/backend/backend.service.ts
+++ Frontend/frontend/src/app/backend/backend.service.ts
@@ -6,6 +6,13 @@ import { Injectable } from '@angular/core';
 export class BackendService {
 
   constructor() {}
+  //Nie wiem czy tu jest http czy https więc zmieńcie jak coś
+  //Nie wiem jaki port czy też jak nazwiecie bramę w dockerze
+  private gateUrl = "http://gate:8080/api"
+  private loginCheckUrl = "/login/check"
+  private tripControllerUrl = "/trip"
+  private tripListUrl = "/trip-list-info"
+  private tripUrl = "/trip-info"
 
   public checkLogin(userName: string): boolean {
     return ['jasio', 'kasia', 'zbysio'].includes(userName.trim())
