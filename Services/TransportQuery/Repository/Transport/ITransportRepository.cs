a91c10cafca8c0ac24dfed9530e752e548204c1a
Agata Geisler <geisler.agata@wp.pl>
2024-05-16 19:38:05 +0200

    Added get method for transport

--- Services/TransportQuery/Repository/Transport/ITransportRepository.cs
+++ Services/TransportQuery/Repository/Transport/ITransportRepository.cs
@@ -5,12 +5,15 @@ namespace TransportQuery.Repository.TransportRepository
 {
     public interface ITransportRepository
     {
-        public List<ConnectionModel> getConnectionGoingTo(string destinationCity);
-        public List<ConnectionModel> getConnectionComingFrom(string destinationCity);
+        public List<ConnectionModel> getConnectionTo(string destination);
+        public List<ConnectionModel> getConnectionFrom(string country);
 
-        public List<TransportModel> getTransportsForConnection(string destinationIds);
+        public List<TransportModel> getTransportsForConnection(int destinationIds);
 
-        public int getNumberOfTakenSeatsForTransport(string transportId);
-        public List<Transport> getTransportsByIds(string departureId, string returnId);
+        public int getNumberOfTakenSeatsForTransport(int transportId);
+        public List<Transport> getTransportsByIds(int departureId, int returnId);
+        
+        public List<Transport> GetTransports();
+        public Transport GetTransport(int transportId);
     }
 }
