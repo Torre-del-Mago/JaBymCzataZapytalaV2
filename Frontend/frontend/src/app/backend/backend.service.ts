5468945cd3cb49223d0c6afba2cde448a746964e
Bezimienii <czerminskijakub@gmail.com>
2024-05-17 01:19:51 +0200

    Changed search component

--- Frontend/frontend/src/app/backend/backend.service.ts
+++ Frontend/frontend/src/app/backend/backend.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as Mocks from '../dto/Mocks';
+import { TripDTO } from '../dto/TripDTO';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BackendService {
+  constructor(private client: HttpClient) {}
 
-  constructor() {}
   //Nie wiem czy tu jest http czy https więc zmieńcie jak coś
   //Nie wiem jaki port czy też jak nazwiecie bramę w dockerze
-  private gateUrl = "http://gate:8080/api"
-  private loginCheckUrl = "/login/check"
-  private tripControllerUrl = "/trip"
-  private tripListUrl = "/trip-list-info"
-  private tripUrl = "/trip-info"
+  private gateUrl = 'http://gate:8080/api';
+  private loginCheckUrl = '/login/check';
+  private tripControllerUrl = '/trip';
+  private tripListUrl = '/trip-list-info';
+  private tripUrl = '/trip-info';
+
+  private currentTrip?: TripDTO
+  private numberOfChildren: number = 0
+  private numberOfAdult: number = 0
 
   public checkLogin(userName: string): boolean {
     return ['jasio', 'kasia', 'zbysio'].includes(userName.trim())
@@ -20,11 +27,44 @@ export class BackendService {
       : false;
   }
 
-  public getInfoForTrips() {}
+  public getInfoForTrips(
+    destination: string,
+    startCity: string,
+    startDate: Date,
+    endDate: Date,
+    numberOfAdults: number,
+    numberOfChildren: number
+  ): TripDTO[] {
+    return Mocks.trips.filter(
+      (t) =>
+        t.Country === destination &&
+        t.ChosenFlight.Departure === startCity &&
+        t.BeginDate >= startDate &&
+        t.EndDate <= endDate
+    );
+    /* return this.client.get<TripsDTO>(this.gateUrl + this.tripListUrl + "?body=") */
+  }
 
   public getInfoForTrip() {}
 
   public reserveOffer() {}
 
   public payOffer() {}
+
+  public setCurrentTrip(trip: TripDTO): void {
+    this.currentTrip = trip;
+  }
+
+  public setNumbers(numberOfChild: number, numberOfAdult: number) {
+    this.numberOfAdult = numberOfAdult
+    this.numberOfChildren = numberOfChild
+  }
+
+  public getNumbers(): number[] {
+    return [this.numberOfAdult, this.numberOfChildren]
+  }
+
+  public getCurrentTrip(): TripDTO | undefined {
+    return this.currentTrip;
+  }
 }
--- Frontend/frontend/src/app/search/search.component.ts
+++ Frontend/frontend/src/app/search/search.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BackendService } from '../backend/backend.service';
+import { TripDTO } from '../dto/TripDTO';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
   destinations = ['', 'Grecja', 'Włochy', 'Hiszpania', 'Egipt'];
@@ -14,7 +17,23 @@ export class SearchComponent {
   endDate = '';
   adults = 1;
   children = 0;
+  result: TripDTO[] = [];
+
+  constructor(private service: BackendService, private router: Router) {}
 
   search(): void {
-  } 
+    this.result = this.service.getInfoForTrips(
+      this.destination,
+      this.startCity,
+      new Date(this.startDate),
+      new Date(this.endDate),
+      this.adults,
+      this.children
+    );
+  }
+
+  async detail(trip: TripDTO): Promise<void> {
+    this.service.setCurrentTrip(trip);
+    await this.router.navigateByUrl('detail');
+  }
 }
