fa21d93ab806586df9fab94d071d2cab2ec431dd
Bezimienii <czerminskijakub@gmail.com>
2024-05-16 22:07:21 +0200

    Added routing for messages

--- Frontend/frontend/src/app/app-routing.module.ts
+++ Frontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SearchComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'detail', component: DetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
