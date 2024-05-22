d7bac013cf7dc934abfb46d3ae639f9b18e43633
Bezimienii <czerminskijakub@gmail.com>
2024-05-22 19:02:26 +0200

    Made changes to saga

--- Services/OfferCommand/OfferSaga.cs
+++ Services/OfferCommand/OfferSaga.cs
@@ -1,7 +1,9 @@
 ﻿using MassTransit;
+using Models.Hotel;
 using Models.Offer;
 using Models.Offer.DTO;
 using Models.Payment;
+using Models.Transport;
 
 namespace OfferCommand
 {
@@ -15,18 +17,33 @@ namespace OfferCommand
         public OfferDTO Offer { get; set; }
 
         public bool MadeReservation {  get; set; }
+        public bool MadeHotelReservation {  get; set; }
+        public bool MadeTransportReservation {  get; set; }
         public bool PaidForReservation {  get; set; }
 
     }
     public class OfferSaga: MassTransitStateMachine<OfferReservation>
     {
+        public State WaitingForHotel {  get; set; }
+        public State WaitingForTransport { get; set; }
         public State ReservedOffer {  get; set; }
-        public State PaidForOffer { get; set; }
 
-        public Event<ReserveOfferEvent> reserveOfferEvent { get; set; }
-        public Event<CheckPaymentEventReply> checkPaymentEvent { get; set; }
+        public Event<ReserveOfferEvent> ReserveOfferEvent { get; set; }
+        public Event<CheckPaymentEventReply> PaymentEvent { get; set; }
+        public Event<ReserveHotelEventReply> ReserveHotelEvent { get; set; }
+        public Event<ReserveTransportEventReply> ReserveTransportEvent { get; set; }
 
-        private void processReservation()
+        private void sendHotelReservation()
+        {
+
+        }
+
+        private void sendTransportReservation()
+        {
+
+        }
+
+        private void startPayment()
         {
 
         }
@@ -41,25 +58,63 @@ namespace OfferCommand
 
         }
 
-        public OfferSaga()
+        private void processHotel()
+        {
+
+        }
+
+        private void processTransport()
+        {
+
+        }
+
+        private void sendPaymentMessage()
+        {
+
+        }
+
+        private void publishReserveOfferResponse()
+        {
+
+        }
+
+        private void publishPaymentResponse()
+        {
+
+        }
+
+        public OfferSaga() 
         {
             InstanceState(x => x.CurrentState);
 
             Initially(
-                When(reserveOfferEvent).
+                When(ReserveOfferEvent).
                 Then(ctx => ctx.Saga.Registration = ctx.Message.Registration).
                 Then(ctx => ctx.Saga.Offer = ctx.Message.Offer).
-                Then(ctx => processReservation()).
-                IfElse(ctx => ctx.Saga.MadeReservation,
-                valid => valid.TransitionTo(ReservedOffer),
-                invalid => invalid.Then(ctx => cancelReservations()).Finalize())
+                Then(ctx => sendHotelReservation()).
+                TransitionTo(WaitingForHotel)
                 );
 
+            During(WaitingForHotel,
+                When(ReserveHotelEvent).
+                Then(ctx => processHotel()).
+                IfElse(ctx => ctx.Saga.MadeHotelReservation,
+valid => valid.Then(ctx => sendTransportReservation()).TransitionTo(ReservedOffer),
+invalid => invalid.Then(ctx => cancelReservations()).Finalize()));
+
+            During(WaitingForTransport,
+                When(ReserveTransportEvent).
+                Then(ctx => processTransport()).
+                IfElse(ctx => ctx.Saga.MadeTransportReservation,
+valid => valid.Then(ctx => publishReserveOfferResponse()).Then(ctx => sendPaymentMessage()).TransitionTo(ReservedOffer),
+invalid => invalid.Then(ctx => cancelReservations()).Finalize()));
+
+
             During(ReservedOffer,
-                When(checkPaymentEvent).
+                When(PaymentEvent).
                 Then(ctx => processPayment()).
                 IfElse(ctx => ctx.Saga.PaidForReservation,
-                valid => valid.Finalize(),
+                valid => valid.Then(ctx => publishPaymentResponse()).Finalize(),
                 invalid => invalid.Then(ctx => cancelReservations()).Finalize()));
 
 
