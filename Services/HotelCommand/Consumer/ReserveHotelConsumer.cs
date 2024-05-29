7eb6ed863c32c190efe98685d5e283f0eb55adb9
Agata Geisler <geisler.agata@wp.pl>
2024-05-29 14:32:16 +0200

    Added consumer service methods

--- Services/HotelCommand/Consumer/ReserveHotelConsumer.cs
+++ Services/HotelCommand/Consumer/ReserveHotelConsumer.cs
@@ -1,14 +1,40 @@
-﻿using MassTransit;
+﻿using HotelCommand.Repository.ReservationEventRepository;
+using HotelCommand.Service;
+using MassTransit;
 using Models.Hotel;
 
 namespace HotelCommand.Consumer
 {
     public class ReserveHotelConsumer : IConsumer<ReserveHotelEvent>
     {
-
-        public Task Consume(ConsumeContext<ReserveHotelEvent> context)
+        private IEventService _eventService;
+        private IPublishEndpoint _publishEndpoint;
+        public ReserveHotelConsumer(IEventService eventService, IPublishEndpoint publishEndpoint)
         {
-            throw new NotImplementedException();
+            _eventService = eventService;
+            _publishEndpoint = publishEndpoint;
+        }
+        public async Task Consume(ConsumeContext<ReserveHotelEvent> context)
+        {
+            bool hasReservedHotel= await _eventService.reserveHotel(context.Message.Reservation);
+            if (!hasReservedHotel) {
+                await _publishEndpoint.Publish(new ReserveHotelEventReply()
+                {
+                    Answer = ReserveHotelEventReply.State.NOT_RESERVED,
+                    CorrelationId = context.Message.CorrelationId
+                });            
+            }
+            await _publishEndpoint.Publish(new ReserveHotelEventReply()
+            {
+                Answer = ReserveHotelEventReply.State.RESERVED,
+                CorrelationId = context.Message.CorrelationId
+            });
+
+            await _publishEndpoint.Publish(new ReserveHotelSyncEvent()
+            {
+                Reservation = context.Message.Reservation
+            });
+
         }
     }
 }
