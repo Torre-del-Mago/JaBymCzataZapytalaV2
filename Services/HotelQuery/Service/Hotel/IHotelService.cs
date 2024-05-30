c0e8817bfbe3113bbd97d29f5da0a94c0d49a6d0
Agata Geisler <geisler.agata@wp.pl>
2024-05-30 11:09:36 +0200

    Draft sync consumers for HotelQuery

--- Services/HotelQuery/Service/Hotel/IHotelService.cs
+++ Services/HotelQuery/Service/Hotel/IHotelService.cs
@@ -5,6 +5,7 @@ namespace HotelQuery.Service.Hotel;
 public interface IHotelService
 {
     HotelsDTO GetHotelsForCriteria(CriteriaForHotels criteria);
-    
     HotelDTO GetHotelForCriteria(CriteriaForHotel criteria);
+    Task<bool> ReserveHotel(HotelReservationDTO dto);
+    Task CancelHotel(int offerId);
 }
