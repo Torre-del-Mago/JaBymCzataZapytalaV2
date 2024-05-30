f6d2fbb2223a4a467fde708f3c35e7cf290fe69e
Bezimienii <czerminskijakub@gmail.com>
2024-05-30 10:47:57 +0200

    Created offer repositories

---
+++ Services/OfferCommand/Repository/EventRepository/EventRepository.cs
@@ -0,0 +1,50 @@
+﻿using OfferCommand.Database;
+
+namespace OfferCommand.Repository.EventRepository
+{
+    public class EventRepository : IEventRepository
+    {
+        private OfferContext _context;
+        public EventRepository(OfferContext context)
+        {
+            _context = context;
+        }
+        public void insertCreatedEvent(int offerId)
+        {
+            insertEvent(offerId, EventTypes.Created);
+        }
+
+        public void insertReservedEvent(int offerId)
+        {
+            insertEvent(offerId, EventTypes.Reserved);
+        }
+        public void insertNotReservedEvent(int offerId)
+        {
+            insertEvent(offerId, EventTypes.NotReserved);   
+        }
+
+        public void insertPaidEvent(int offerId)
+        {
+            insertEvent(offerId, EventTypes.Paid);
+        }
+        public void insertNotPaidInTimeEvent(int offerId)
+        {
+            insertEvent(offerId, EventTypes.NotPaidInTime);
+        }
+
+        public void insertRemovedEvent(int offerId)
+        {
+            insertEvent(offerId, EventTypes.Removed);
+        }
+
+        private void insertEvent(int offerId, string eventType) {
+
+            _context.Events.Add(new Database.Tables.OfferEvent()
+            {
+                OfferId = offerId,
+                EventType = eventType,
+                TimeStamp = DateTime.Now
+            });
+        }
+    }
+}
---
+++ Services/OfferCommand/Repository/EventRepository/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using Models.Offer.DTO;
+
+namespace OfferCommand.Repository.EventRepository
+{
+    public interface IEventRepository
+    {
+        public void insertCreatedEvent(int offerId);
+
+        public void insertReservedEvent(int offerId);
+        public void insertNotReservedEvent(int offerId);
+        public void insertRemovedEvent(int offerId);
+        public void insertPaidEvent(int offerId);
+        public void insertNotPaidInTimeEvent(int offerId);
+    }
+}
