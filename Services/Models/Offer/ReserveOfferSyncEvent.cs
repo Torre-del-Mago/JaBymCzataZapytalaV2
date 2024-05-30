1e8cf6b8e051d0d4bc35ab96a7d8fb599db2c29c
Bezimienii <czerminskijakub@gmail.com>
2024-05-30 10:58:50 +0200

    Changed models

--- Services/Models/Offer/ReserveOfferSyncEvent.cs
+++ Services/Models/Offer/ReserveOfferSyncEvent.cs
@@ -5,8 +5,6 @@ namespace Models.Offer
 {
     public class ReserveOfferSyncEvent : EventModel
     {
-        public int Registration { get; set; }
-
         public OfferDTO Offer { get; set; }
     }
 }
--- Services/Models/Offer/ReservedOfferSyncEvent.cs
+++ Services/Models/Offer/ReservedOfferSyncEvent.cs
@@ -4,5 +4,13 @@ namespace Models.Offer
     public class ReservedOfferSyncEvent : EventModel
     {
         public int OfferId { get; set; }
+
+        public enum State
+        {
+            RESERVED,
+            NOT_RESERVED
+        };
+
+        public State Answer { get; set; }
     }
 }
