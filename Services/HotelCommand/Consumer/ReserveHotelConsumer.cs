36b56e4b870620deae6f3833059a9e13b7ea5c0b
KKucht <krzysztof.k2001.01@wp.pl>
2024-05-31 11:17:14 +0200

    Repair in ReserveHotelConsumer

--- Services/HotelCommand/Consumer/ReserveHotelConsumer.cs
+++ Services/HotelCommand/Consumer/ReserveHotelConsumer.cs
@@ -10,25 +10,31 @@ namespace HotelCommand.Consumer
     {
         public async Task Consume(ConsumeContext<ReserveHotelEvent> context)
         {
+            Console.WriteLine("Get ReserveHotelEvent");
             var hasReservedHotel= await eventService.ReserveHotel(context.Message.Reservation);
             if (!hasReservedHotel) {
                 await context.RespondAsync(new ReserveHotelEventReply()
                 {
                     Answer = ReserveHotelEventReply.State.NOT_RESERVED,
                     CorrelationId = context.Message.CorrelationId
-                });            
+                });
+                
             }
-            await context.RespondAsync(new ReserveHotelEventReply()
+            else
             {
-                Answer = ReserveHotelEventReply.State.RESERVED,
-                CorrelationId = context.Message.CorrelationId
-            });
+                await context.RespondAsync(new ReserveHotelEventReply()
+                {
+                    Answer = ReserveHotelEventReply.State.RESERVED,
+                    CorrelationId = context.Message.CorrelationId
+                });
+            }
+            Console.WriteLine("Send ReserveHotelEventReply");
 
             await publishEndpoint.Publish(new ReserveHotelSyncEvent()
             {
                 Reservation = context.Message.Reservation
             });
-
+            Console.WriteLine("Send ReserveHotelSyncEvent");
         }
     }
 }
