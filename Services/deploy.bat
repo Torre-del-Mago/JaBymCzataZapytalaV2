c3645cee237871549d75721e9fc525338d7df4ae
KKucht <krzysztof.k2001.01@wp.pl>
2024-06-05 13:21:11 +0200

    add front

--- Services/deploy.bat
+++ Services/deploy.bat
@@ -2,11 +2,13 @@
 
 docker-compose build
 
-set services=gate hotelcommand hotelquery login offercommand offerquery payment transportcommand transportquery trip
+set services=gate hotelcommand hotelquery login offercommand offerquery payment transportcommand transportquery trip front
 
 set username=kkucht
 
 for %%i in (%services%) do (
     docker tag %%i %username%/rsww_184543_%%i:latest
     docker push %username%/rsww_184543_%%i:latest
+    docker rmi %%i
+    docker rmi %username%/rsww_184543_%%i:latest
 )
--- Services/deploy.sh
+++ Services/deploy.sh
@@ -2,7 +2,7 @@
 
 docker-compose build
 
-services=("gate" "hotelcommand" "hotelquery" "login" "offercommand" "offerquery" "payment" "transportcommand" "transportquery" "trip")
+services=("gate" "hotelcommand" "hotelquery" "login" "offercommand" "offerquery" "payment" "transportcommand" "transportquery" "trip" "front")
 
 username="kkucht"
 
@@ -10,4 +10,6 @@ for service in "${services[@]}"
 do
   docker tag $service $username/rsww_184543_$service:latest
   docker push $username/rsww_184543_$service:latest
+  docker rmi $service
+  docker rmi $username/rsww_184543_$service:latest
 done
