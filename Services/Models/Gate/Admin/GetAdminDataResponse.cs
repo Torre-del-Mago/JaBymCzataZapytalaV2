7014797e87956251377a731cf8786142fde92dd7
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2024-06-13 21:08:44 +0200

    Admin model enlarged for point 10

--- Services/Models/Gate/Admin/GetAdminDataResponse.cs
+++ Services/Models/Gate/Admin/GetAdminDataResponse.cs
@@ -1,4 +1,5 @@
 using Models.Admin.DTO;
+using Models.Gate.TravelAgency;
 
 namespace Models.Gate
 {
@@ -8,5 +9,6 @@ namespace Models.Gate
         public TopRoomTypesDTO TopRoomTypesDto { get; set; }
         public TopDepartureDTO TopDepartureDto { get; set; }
         public TopDestinationDTO TopDestinationDto { get; set; }
+        public LastTravelAgencyChangesResponse LastTravelAgencyChangesResponse { get; set; }
     }
 }
